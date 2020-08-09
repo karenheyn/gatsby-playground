73c1e0012e507820c2a0e7af3347aad7bc668957
karenheyn <karenheyn@gmail.com>
2020-08-08 21:11:46 -0400

    fixe double header in contact, added styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,23 +3,47 @@ import { Link } from "gatsby"
 // import "./header.module.scss"
 import HeaderStyles from "./header.module.scss"
 const Header = () => (
-  <header>
-    <h1>Karen Heyn</h1>
+  <header className={HeaderStyles.header}>
+    <Link
+      to="/"
+      className={HeaderStyles.title}
+      activeClassName={HeaderStyles.activeNavItem}
+    >
+      Karen Heyn
+    </Link>
     <nav>
-      <ul>
+      <ul className={HeaderStyles.navList}>
         <li>
-          <Link to="/" className={HeaderStyles.link}>
+          <Link
+            to="/"
+            className={HeaderStyles.navItem}
+            activeClassName={HeaderStyles.activeNavItem}
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link
+            to="/about"
+            className={HeaderStyles.navItem}
+            activeClassName={HeaderStyles.activeNavItem}
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link
+            to="/contact"
+            className={HeaderStyles.navItem}
+            activeClassName={HeaderStyles.activeNavItem}
+          >
+            Contact
+          </Link>
         </li>
         <li>
-          <Link to="/blog">Blog</Link>
+          <Link to="/blog" className={HeaderStyles.navItem}>
+            Blog
+          </Link>
         </li>
       </ul>
     </nav>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,6 @@ import Layout from "../components/layout"
 const ContactPage = () => (
   <div>
     <Layout>
-      <Header />
       <h1>Contact Page</h1>
       <p>karenheyn@gmail.com</p>
       <a href="https://www.linkedin.com/in/karenheyn" target="blank">
