83f98edbe3ce4392273e5bd2a70cb2fc3e635293
karenheyn <karenheyn@gmail.com>
2020-08-12 20:33:08 -0400

    added scss styling

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import blogStyles from "./blog.module.scss"
 import Layout from "../components/layout"
 import { useStaticQuery, graphql, Link } from "gatsby"
 const BlogPage = () => {
@@ -27,10 +27,10 @@ const BlogPage = () => {
       <Layout>
         <h1>My blog</h1>
         <p>Posts will show up here later on.</p>
-        <ol>
+        <ol className={blogStyles.posts}>
           {posts.allMarkdownRemark.edges.map(edge => {
             return (
-              <li>
+              <li className={blogStyles.post}>
                 <Link to={edge.node.fields.slug}>
                   <h2>{edge.node.frontmatter.title}</h2>
                   <p>{edge.node.frontmatter.date}</p>
