1b5d8e3e9941ddb42b5743d278761a43c2277ee3
karenheyn <karenheyn@gmail.com>
2020-10-17 16:37:37 -0400

    starting mobile nav

--- src/components/header.js
+++ src/components/header.js
@@ -36,6 +36,53 @@ const Header = () => {
         <span></span>
         <span></span>
       </div>
+      {menuActive ? (
+        <nav>
+          <ul className={HeaderStyles.navList}>
+            <li>
+              <Link
+                to="/"
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/work"
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+              >
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to="/blog" className={HeaderStyles.navItem}>
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      ) : null}
       {/* <nav>
         <ul className={HeaderStyles.navList}>
           <li>
