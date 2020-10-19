012e5aa6df02ba8cb54e8acfefab73535b6d365f
karenheyn <karenheyn@gmail.com>
2020-10-18 22:43:24 -0400

    removed hamburger menu in desktop

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 // import "./header.module.scss"
 import HeaderStyles from "./header.module.scss"
@@ -13,6 +13,16 @@ const Header = () => {
       }
     }
   `)
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 768)
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 768)
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia)
+    return () => window.removeEventListener("resize", updateMedia)
+  })
   const [menuActive, setMenuState] = useState(false)
   return (
     <header
@@ -26,18 +36,19 @@ const Header = () => {
         {/* {data.site.siteMetadata.title} */}
         <img src={Logo} height="60" />
       </Link>
-
-      <div
-        onClick={() => setMenuState(!menuActive)}
-        className={`${HeaderStyles.navIcon3} ${
-          menuActive ? HeaderStyles.open : ""
-        }`}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+      {isDesktop ? null : (
+        <div
+          onClick={() => setMenuState(!menuActive)}
+          className={`${HeaderStyles.navIcon3} ${
+            menuActive ? HeaderStyles.open : ""
+          }`}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      )}
       {menuActive ? (
         <nav>
           <ul>
@@ -88,52 +99,53 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-      ) : null}
-      {/* <nav>
-        <ul className={HeaderStyles.navList}>
-          <li>
-            <Link
-              to="/"
-              className={HeaderStyles.navItem}
-              activeClassName={HeaderStyles.activeNavItem}
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/about"
-              className={HeaderStyles.navItem}
-              activeClassName={HeaderStyles.activeNavItem}
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/work"
-              className={HeaderStyles.navItem}
-              activeClassName={HeaderStyles.activeNavItem}
-            >
-              Work
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/contact"
-              className={HeaderStyles.navItem}
-              activeClassName={HeaderStyles.activeNavItem}
-            >
-              Contact
-            </Link>
-          </li>
-          <li>
-            <Link to="/blog" className={HeaderStyles.navItem}>
-              Blog
-            </Link>
-          </li>
-        </ul>
-      </nav> */}
+      ) : (
+        <nav>
+          <ul className={HeaderStyles.navList}>
+            <li>
+              <Link
+                to="/"
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/work"
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+              >
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to="/blog" className={HeaderStyles.navItem}>
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   )
 }
